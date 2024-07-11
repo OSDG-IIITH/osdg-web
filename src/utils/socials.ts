50d35b35a4d35b2023a2b357733c862910f1601e
Ankith <itsankith26@gmail.com>
2024-07-11 22:39:54 +0530

    misc: Add discord to socials

--- src/utils/socials.ts
+++ src/utils/socials.ts
@@ -1,5 +1,6 @@
 import { IconDefinition } from "@fortawesome/free-brands-svg-icons";
 import {
+  faDiscord,
   faFacebook,
   faGithub,
   faLinkedin,
@@ -8,6 +9,7 @@ import {
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const socials: [string, IconDefinition][] = [
+  ["https://discord.gg/S74Td7fbHN", faDiscord],
   ["https://www.facebook.com/groups/185567594878116", faFacebook],
   ["https://github.com/OSDG-IIITH", faGithub],
   ["https://www.linkedin.com/company/74330374", faLinkedin],
